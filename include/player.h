116d43ed0aa2466a8cd9274fe7408790135fb2c4
David Martin <davidmartin@Davids-MacBook-Pro.local>
2026-01-04 18:56:59 -0600

    Added player and movement

---
+++ include/player.h
@@ -0,0 +1,15 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <SDL2/SDL.h>
+
+typedef struct {
+    int x, y; // grid position
+    SDL_Texture* sprite;
+} Player;
+
+int init_player(Player* player, SDL_Renderer* renderer);
+void handle_player_input(Player* player, const Uint8* keystates);
+void draw_player(Player* player, SDL_Renderer* renderer);
+
+#endif
--- include/render.h
+++ include/render.h
@@ -3,6 +3,9 @@
 
 #include <SDL2/SDL.h>
 
+#define TILE_WIDTH 64
+#define TILE_HEIGHT 32
+
 int load_tile_textures(SDL_Renderer* renderer);
 void draw_map(SDL_Renderer* renderer);
 
